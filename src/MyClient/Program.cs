42fcac25e1db9e2ed3265f5736514c087b5e1f89
wk-j <somnuk.wk@gmail.com>
2019-05-09 17:00:44 +0700

    Added client

---
+++ src/MyClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
